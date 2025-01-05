420e131b97e9f879095afa7f026e7f865f8d2ba7
naufalzp <naufalarya0@gmail.com>
2025-01-05 14:44:55 +0700

    feat: Organize API routes under versioning and add disaster management endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,26 @@
 <?php
 
+use App\Http\Controllers\API\Auth\AuthController;
+use App\Http\Controllers\API\DisasterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+Route::prefix('v1')->group(function () {
+    Route::prefix('auth')->group(function () {
+        Route::post('login', [AuthController::class, 'login']);
+        Route::post('register', [AuthController::class, 'register']);
+
+        Route::middleware('auth:api')->group(function () {
+            Route::get('user', [AuthController::class, 'user']);
+            Route::post('logout', [AuthController::class, 'logout']);
+        });
+    });
+
+    Route::middleware('auth:api')->group(function () {
+        Route::get('disasters', [DisasterController::class, 'index']);
+        Route::post('disasters', [DisasterController::class, 'store']);
+        Route::get('disasters/{id}', [DisasterController::class, 'show']);
+        Route::put('disasters/{id}', [DisasterController::class, 'update']);
+        Route::delete('disasters/{id}', [DisasterController::class, 'destroy']);
+    });
+});
